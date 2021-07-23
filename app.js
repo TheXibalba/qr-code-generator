65586eaa7375b61294ad32a71e5efac49d188445
Yuvraj Pawar <yurajpawar99@gmail.com>
2021-07-23 15:05:27 +0530

    Fixed the server port

--- app.js
+++ app.js
@@ -1,40 +1,23 @@
-const express=require('express');
-const app=express();
+const express = require("express");
+const app = express();
 
-
-app.use(express.static(__dirname+'/public'));
-app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.post("/",(req,res)=>{
-    const qrUrl=req.body.url;
-    console.log(qrUrl);
-    res.render("genPage",{
-        QR: qrUrl
-    });
+app.post("/", (req, res) => {
+  const qrUrl = req.body.url;
+  console.log(qrUrl);
+  res.render("genPage", {
+    QR: qrUrl,
+  });
 });
 
-
-app.get("/",(req,res)=>{
-res.render("index");
-
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000,()=>{
-    console.log("Server is running!");
-})
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
